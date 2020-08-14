07c307012a1b95fe84fde33076b594f08a3b4723
Sameerna Joshi <sameernajoshi@gmail.com>
2020-08-14 17:52:04 +0530

    Updates

---
+++ precache-manifest.ab7cac5075d8fde16497a0943abba4d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e33263e84aa46d9e33d0d9d1c756a61",
+    "url": "/React_Recipe/index.html"
+  },
+  {
+    "revision": "5bbef9b9b4377c9fb169",
+    "url": "/React_Recipe/static/css/2.ca8e03c8.chunk.css"
+  },
+  {
+    "revision": "38b62b75e97e9c40c9ec",
+    "url": "/React_Recipe/static/css/main.85a0e10b.chunk.css"
+  },
+  {
+    "revision": "5bbef9b9b4377c9fb169",
+    "url": "/React_Recipe/static/js/2.7a010765.chunk.js"
+  },
+  {
+    "revision": "38b62b75e97e9c40c9ec",
+    "url": "/React_Recipe/static/js/main.bb569e81.chunk.js"
+  },
+  {
+    "revision": "9a99fdbc91f3bae4ead2",
+    "url": "/React_Recipe/static/js/runtime~main.e9e69280.js"
+  }
+]);
